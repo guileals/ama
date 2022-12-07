f9afde5526880ef1628cdebc6cf3f259fcb60cee
Guilherme Santos <g.santos@outlook.com>
2022-12-07 17:59:10 -0300

    config.host apagado

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << 152.70.213.102
+  # config.hosts << 152.70.213.102
 
   # Code is not reloaded between requests.
   config.cache_classes = true
